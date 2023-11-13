edc0e6047db7ad02f9109ca9cbd43e60dd4fe7ce
gcusaaa <gcusaaa@gmail.com>
2023-11-13 15:41:17 +0200

    folder structure

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById('calculateBtn')
+
+
+button.addEventListener('click', () => {
+    const height = parseInt(document.getElementById('height').value);
+    const weight = parseInt(document.getElementById('weight').value);
+    const result = document.getElementById('answer');
+    let height_status = false, weight_status = false
+})
